fb82dffcf06795d729d5e4b0def4b20f45a022db
Aensheng <aensheng@gmail.com>
2019-10-12 04:16:43 +1100

    moving shop data to firebase and using HOC withSpinner

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,16 @@
-import SHOP_DATA from './shop.data'
+import ShopActionTypes from './shop.types' 
 
 const Initial_state = {
-    collections: SHOP_DATA
+    collections: null
 }
 
 const shopReducer = (state=Initial_state, action) => {
     switch(action.type){
+        case ShopActionTypes.UPDATE_COLLECTIONS:
+            return{
+                ...state,
+                collections: action.payload
+            }
         default:
             return state
     }
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -10,12 +10,13 @@ export const selectCollections = createSelector(
 //convert collection object to array
 export const selectCollectionsForPreview = createSelector(
     [selectCollections],
-    collections => Object.keys(collections).map(key => collections[key])
+    collections => 
+    collections ? Object.keys(collections).map(key => collections[key]) : []
 )
 
 // get data from collection object
 export const selectCollection = collectionUrlParam =>
     createSelector(
         [selectCollections],
-        collections => collections[collectionUrlParam]
+        collections => (collections ? collections[collectionUrlParam] : null)
     )
